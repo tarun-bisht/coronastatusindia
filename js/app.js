5400dfbd87278338a1defdc409d39caf5980c3ae
tarun-bisht <tarunbishttarun.11@gmail.com>
2020-03-30 19:08:10 +0530

    table duplication fixed

--- js/app.js
+++ js/app.js
@@ -155,6 +155,12 @@ function load_from_api(value)
         });
     });
 }
+function clear_table()
+{
+    document.getElementById("table-body").innerHTML="";
+}
+
+
 function refresh()
 {
     fetch(`${url}/scapre-it`).then(function(response)
